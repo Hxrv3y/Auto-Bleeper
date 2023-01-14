32a1227e5f389e8cefd25feb5e6f8090c4a49ee8
Wolfhill09 <harvey.akhurst@icloud.com>
2023-01-14 16:15:46 +0000

    Add: Python script

---
+++ bleeper.py
@@ -0,0 +1,33 @@
+import speech_recognition as sr
+import winsound
+
+# Set up the recognizer
+r = sr.Recognizer()
+mic = sr.Microphone()
+
+# Set up the list of bad words
+bad_words = ["fuck", "shit"]
+
+# Main loop
+while True:
+    # Listen for speech
+    print("Listening...")
+    with mic as source:
+        audio = r.listen(source)
+
+    # Try to recognize the speech
+    try:
+        text = r.recognize_google(audio)
+        print(f"You said: {text}")
+
+        # Check for bad words
+        for word in bad_words:
+            if word in text.lower():
+                # Play a bleep sound
+                winsound.PlaySound("bleep.wav", winsound.SND_FILENAME)
+                break
+
+    except sr.UnknownValueError:
+        print("Could not understand audio")
+    except sr.RequestError as e:
+        print(f"Error recognizing speech: {e}")
